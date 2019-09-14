65d2d9d55db468e9e64dcc3aec0f3aeaf992e825
maksimmanzhai <bender23max@gmail.com>
2019-09-15 00:07:46 +0300

    Adding code for hash with vowel letters

--- lesson2_3.rb
+++ lesson2_3.rb
@@ -5,6 +5,7 @@ index = 0
 
 loop do
   break if (array[index] + array[index + 1]) > 100
+
   array.push(array[index] + array[index + 1])
   index += 1
 end
---
+++ lesson2_4.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+hash = {}
+index = 1
+('a'..'z').each do |letter|
+  hash[letter] = index if (letter.count 'aeiouy') == 1
+  index += 1
+end
