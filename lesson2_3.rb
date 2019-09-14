a01865447158889819858cd48dc30b8b474a2c1c
maksimmanzhai <bender23max@gmail.com>
2019-09-14 22:29:07 +0300

    Adding code with Fibonacci numbers

---
+++ lesson2_3.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+array = Array.new(2, 1)
+index = 0
+
+loop do
+  break if (array[index] + array[index + 1]) > 100
+  array.push(array[index] + array[index + 1])
+  index += 1
+end
